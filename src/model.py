c5566c5eabf93e4bb95e56bf7fde11742d1cddec
sahgelerm <sahgelerm@mail.ru>
2026-03-26 20:16:32 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,14 @@
+from tensorflow.keras import layers, models
+
+def build_model(num_classes):
+    model = models.Sequential([
+        layers.Conv2D(32, (3,3), activation='relu', input_shape=(224,224,3)),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, (3,3), activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
 
